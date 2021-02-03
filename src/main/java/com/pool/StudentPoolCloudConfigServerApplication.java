86555ff4f5f129cbb7486ec60999d2061c184596
shiva508 <dasarishiva1@gmail.com>
2021-02-03 17:21:31 +0530

    Initial commit

---
+++ src/main/java/com/pool/StudentPoolCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class StudentPoolCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPoolCloudConfigServerApplication.class, args);
+	}
+
+}
